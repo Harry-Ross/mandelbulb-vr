ad8a3c90853f65117c75880c362271a9fbbac98d
Harry Ross <hazross@hotmail.com>
2021-07-22 11:22:30 +1000

    initia;

---
+++ Assets/Scripts/Mandelbrot2D.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mandelbrot2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	int escapeSteps (float x, float y, int detail) {
+		int count = 0;
+		float z = 0;
+		float rootZ = Mathf.Sqrt(z);
+
+		float newX = x;
+		float newY = y;
+
+		while (rootZ < 2.0 && count < detail) {
+			// Variable and equation definition
+			float xx = newX * newX;
+			float yy = newY * newY;
+			float twoab = 2 * newX * newY;
+			newX = xx - yy + x;
+			newY = twoab + y;
+
+			// Defines the distance
+			z = Mathf.Sqrt(twoab * twoab + (xx - yy) * (xx - yy));
+			rootZ = Mathf.Sqrt(z);
+
+			// Increments the count
+			count++;
+		}
+
+		return count;
+	}
+}
---
+++ Assets/Scripts/MandelbulbCreator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MandelbulbCreator : MonoBehaviour {
+	public Material material;
+	public const int power = 6;
+
+	public GameObject cube;	
+
+	// Use this for initialization
+	void Start () {
+		const int iterations = 25;
+		const int detail = 25;
+		Debug.Log("hello");
+		for (int x = -iterations; x < iterations; x++) {
+			for (int y = -iterations; y < iterations; y++) {
+				for (int z = -iterations; z < iterations; z++) {
+					if (inMandelBulb(new Vector3(x / detail, y / detail, z / detail))) {
+						Instantiate(cube, new Vector3(x,y,z), transform.rotation);
+					}
+				}
+			}
+		}
+		Debug.Log("done");
+	}
+	// your code is shit harry
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	bool inMandelBulb (Vector3 point) {
+		int count = 0;
+		Vector3 v = point;
+		float r = 0;
+		while (count < 25 && r < power) {
+			r = Mathf.Sqrt(Mathf.Pow(v.x, 2) + Mathf.Pow(v.y, 2) + Mathf.Pow(v.z, 2));
+            v = vectorPower(v, power);
+			count++;
+		}
+		return(r < power);
+	}
+
+	Vector3 vectorPower (Vector3 v, int n) {
+		float r = Mathf.Sqrt(Mathf.Pow(v.x, 2) + Mathf.Pow(v.y, 2) + Mathf.Pow(v.z, 2));
+		
+		float circleWithALineThroughIt = Mathf.Atan2(v.y, v.x);
+		float theta = Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(v.x, 2) + Mathf.Pow(v.y, 2)), v.z);
+
+		float x = Mathf.Sin(n * theta) * Mathf.Cos(n * circleWithALineThroughIt) * Mathf.Pow(r, n);
+		float y = Mathf.Sin(n * theta) * Mathf.Sin(n * circleWithALineThroughIt) * Mathf.Pow(r, n);
+		float z = Mathf.Cos(n * theta) * Mathf.Pow(r, n);
+
+		Vector3 newVector = new Vector3(x, y, z);
+		return newVector;
+	}
+}
